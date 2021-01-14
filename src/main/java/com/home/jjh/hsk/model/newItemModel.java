a73753fb7cb3df6541df3d6eb17148b175f0ed11
허성근 <asakuma@live.com>
2021-01-14 21:57:50 +0900

    -소스 머지-누락 파일

---
+++ src/main/java/com/home/jjh/hsk/model/newItemModel.java
@@ -0,0 +1,26 @@
+package com.home.jjh.hsk.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ **/
+@Getter
+@Setter
+@ToString
+public class newItemModel {
+    //goods_id
+    private String shopItemPriceSale;
+    private String shopTitle ;
+    private String updateTime;
+    private String shopItemUrl;
+    private String shopItemName;
+    private String shopItemDetailUrl;
+    private String shopItemPriceOriginal;
+    private String itemCategori ;
+    private String itemDetailUrl ;
+
+
+}
